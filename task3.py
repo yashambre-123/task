d7a102c9cb6206489936925eb2f26f0f1ebf94f7
Yash Ulhas Ambre <82760930+yashambre-123@users.noreply.github.com>
2023-11-29 20:31:25 +0530

    new working code

--- task3.py
+++ task3.py
@@ -16,11 +16,10 @@ from cv_bridge import CvBridge, CvBridgeError
 
 print("start!!")
 
-# connection_string = 'udp:127.0.0.1:14550'
-vehicle = connect('udp:127.0.0.1:14550', baud=921600, wait_ready=False)
-vehicle.wait_ready(True, raise_exception=False)
+connection_string = 'udp:127.0.0.1:14550'
+vehicle = connect(connection_string, baud=921600, wait_ready=False)
+vehicle.wait_ready(True, timeout=300)
 
-print("yash")
 
 # changing the default parameters
 vehicle.parameters['PLND_ENABLED'] = 1
@@ -35,28 +34,50 @@ takeoff_height = 5
 
 # newimg_pub = rospy.Publisher("/iris_demo/usb_cam/image_new", Image, queue_size=10)
 
-id_to_find = 0
-marker_size = 20
+id_to_find = 55
+marker_size = 100
 
-aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
-parameters = aruco.DetectorParameters()
+# aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+aruco_type = "DICT_4X4_100"
+
+aruco_dict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+
+parameters = aruco.DetectorParameters_create()
 
 horizontal_res = 640
 vertical_res = 480
-horizontal_fov = 62.2 * (math.pi / 180)
-vertical_fov = 48.8 * (math.pi / 180)
-
-
-found_count = 0
-not_found_count = 0
+horizontal_fov = 1.047 * (math.pi / 180)
+# vertical_fov = 48.8 * (math.pi / 180)
+vertical_fov = 3.6 * (math.pi / 180)
 
 dist_coeff = [0.0, 0.0, 0.0, 0.0, 0]
 camera_matrix = [[1861.635, 0.0, 648.5], [0.0, 1861.635, 360.5], [0.0, 0.0, 1.0]]
 np_camera_matrix = np.array(camera_matrix)
 np_dist_coeff = np.array(dist_coeff)
-
-time_last = 0
-time_to_wait = 0.1
 
 # vehicle changes to required modes, takes off to the required height 
 def arm_and_takeoff(target_altitude):
@@ -113,98 +134,83 @@ def send_land_message(x, y):
     vehicle.send_mavlink(msg)
     vehicle.flush()
 
-# function to detect aruco marker, and call send_land_message accordingly based on the position
+
 def image_callback(ros_image):
-    global bridge, not_found_count, found_count, time_last, time_to_wait, id_to_find
-    # try:
-    #     cv_image = bridge.imgmsg_to_cv2(ros_image)
-    # except CvBridgeError as e:
-    #     print(e)
+    global bridge, id_to_find
     
-    if ((time.time() - time_last) > time_to_wait):
-        # npp_data = []
-        # for i in ros_image:
-        #     b = int(i)
-        #     npp_data.append(b)
-        # # np_data = rnp.numpify(ros_image)
-        # np_data = rnp.numpify(npp_data)
-        
-        try:
-            cv_image = bridge.imgmsg_to_cv2(ros_image)
-        except CvBridgeError as e:
-            print(e)
-        gray_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    try:
+        cv_image = bridge.imgmsg_to_cv2(ros_image)
+    except CvBridgeError as e:
+        print(e)
+    
+    gray_img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
 
-        ids = ''
-        (corners, ids, rejected) = aruco.detectMarkers(image=gray_img, dictionary=aruco_dict, parameters=parameters)
+    ids = ''
+    (corners, ids, rejected) = aruco.detectMarkers(image=gray_img, dictionary=aruco_dict, parameters=parameters)
 
-        try:
-            if ids is not None:
-                if (ids[0]==id_to_find):
-                    ret = aruco.estimatePoseSingleMarkers(corners, marker_size, cameraMatrix=np_camera_matrix, distCoeffs=np_dist_coeff)
-                    (rvec, tvec) = (ret[0][0,0,:], ret[1][0,0,:])
-                    x = '{:.2f}'.format(tvec[0])
-                    y = '{:.2f}'.format(tvec[1])
-                    z = '{:.2f}'.format(tvec[2])
+    print(ids)
+    
+    if ids is not None:
+        if (ids[0]==id_to_find):
+            ret = aruco.estimatePoseSingleMarkers(corners, marker_size, cameraMatrix=np_camera_matrix, distCoeffs=np_dist_coeff)
+            (rvec, tvec) = (ret[0][0,0,:], ret[1][0,0,:])
+            x = '{:.2f}'.format(tvec[0])
+            y = '{:.2f}'.format(tvec[1])
+            z = '{:.2f}'.format(tvec[2])
 
-                    y_sum = 0
-                    x_sum = 0
+            y_sum = 0
+            x_sum = 0
 
-                    x_sum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
-                    y_sum = corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1]
+            x_sum = corners[0][0][0][0] + corners[0][0][1][0] + corners[0][0][2][0] + corners[0][0][3][0]
+            y_sum = corners[0][0][0][1] + corners[0][0][1][1] + corners[0][0][2][1] + corners[0][0][3][1]
 
-                    x_avg = x_sum / 4
-                    y_avg = y_sum / 4
+            x_avg = x_sum / 4
+            y_avg = y_sum / 4
 
-                    x_ang = (x_avg - horizontal_res*0.5) * horizontal_fov/horizontal_res
-                    y_ang = (y_avg - vertical_res*0.5) * vertical_fov/vertical_res
+            x_ang = (x_avg - horizontal_res*0.5) * horizontal_fov/horizontal_res
+            y_ang = (y_avg - vertical_res*0.5) * vertical_fov/vertical_res
 
-                    if vehicle.mode!='LAND':
-                        vehicle.mode = VehicleMode('LAND')
-                        while vehicle.mode != 'LAND':
-                            time.sleep(1)
-                        print("vehicle in LAND mode")
-                        send_land_message(x_ang, y_ang)
-                    else:
-                        send_land_message(x_ang, y_ang)
-                    
-                    # marker_position = 'MARKER POSITION: x='+x+' y='+y+' z='+z
-                    # aruco.drawDetectedMarkers(np_data, corners)
-                    # aruco.drawAxis(np_data, np_camera_matrix, np_dist_coeff, rvec, tvec, 10)
-
-                    # cv2.putText(np_data, marker_position, [10, 50], 0, 0.5, (255, 0, 0), thickness=1)
-                    # print(marker_position)
-                    # print("FOUND COUNT:"+str(found_count)+' not found count:'+str(not_found_count))
-                    found_count = found_count + 1
-                else:
-                    not_found_count = not_found_count + 1
+            if vehicle.mode!='LAND':
+                vehicle.mode = VehicleMode('LAND')
+                while vehicle.mode != 'LAND':
+                    time.sleep(1)
+                print("vehicle in LAND mode")
+                send_land_message(x_ang, y_ang)
             else:
-                not_found_count = not_found_count + 1
-        except Exception as e:
-            print("Target not found")
-            print(e)
-            not_found_count = not_found_count + 1
-        
-        # new_msg = rnp.msgify(Image, np_data, encoding="rgb8")
-        # newimg_pub.publish(new_msg)
-        time_last = time.time()
-    else:
-        return None        
+                send_land_message(x_ang, y_ang)
+            
+            # marker_position = 'MARKER POSITION: x='+x+' y='+y+' z='+z
+            marker_position = (x,y)
+            aruco.drawDetectedMarkers(cv_image, corners)
+            aruco.drawAxis(cv_image, np_camera_matrix, np_dist_coeff, rvec, tvec, 10)
 
+            # cv2.putText(cv_image, str(marker_position), [10, 50], 0, 0.5, (255, 0, 0), thickness=1)
+            print(marker_position)
+            # print("FOUND COUNT:"+str(found_count)+' not found count:'+str(not_found_count))
+            # found_count = found_count + 1
+        else:
+            print("marker is not the intended one")
+    else:
+        print("no marker detected")
+    
+    cv2.imshow("Image Monitor", cv_image)
+    
 
 def main():
+    print("yash")
     rospy.init_node("task3_node", anonymous=True)
     print("start")
-    arm_and_takeoff(5)
-    time.sleep(1)
-    send_local_ned_velocity(velocity, 0, 0)
-    time.sleep(1)
     sub = rospy.Subscriber("/iris_demo/usb_cam/image_raw", Image, image_callback)
     rospy.spin()
 
 
-if __name__=="__main__":
+if __name__=='__main__':
     try:
+        arm_and_takeoff(takeoff_height)
+        time.sleep(20)
+        print("local ned")
+        send_local_ned_velocity(velocity, velocity, velocity)
+        time.sleep(1)
         main()
     except rospy.ROSInterruptException:
         pass
